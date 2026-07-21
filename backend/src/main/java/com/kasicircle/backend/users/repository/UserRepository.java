8958c8d834be96276ea5ef5d403d8ff300dc9286
Thando Mthethwa <njabulodhladla8@gmail.com>
2026-07-21 23:46:40 +0200

    feat: implement user repository

---
+++ backend/src/main/java/com/kasicircle/backend/users/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.kasicircle.backend.users.repository;
+
+import com.kasicircle.backend.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
