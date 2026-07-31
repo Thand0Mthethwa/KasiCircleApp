fb9664ffe0b5a887a15eb2e0e1ef0a9c7acf35b2
Thando Mthethwa <njabulodhladla8@gmail.com>
2026-07-31 16:56:52 +0200

    feat(business):implemented BusinessRepository

---
+++ backend/src/main/java/com/kasicircle/backend/businesses/repository/BusinessRepository.java
@@ -0,0 +1,11 @@
+package com.kasicircle.backend.businesses.repository;
+
+import com.kasicircle.backend.businesses.domain.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BusinessRepository extends JpaRepository<Business, UUID> {
+}
